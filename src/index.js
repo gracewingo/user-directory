ae0afc9e925de9b06330ab51c00f0d55f0b34be1
Grace Wingo <grace.wingo@gmail.com>
2020-02-12 13:37:27 -0500

    remove unused leaflet files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'leaflet/dist/leaflet.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
